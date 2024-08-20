3e73ef0944723bdd6ef64e5d752edd80350034e8
Maurin Audenard <maurin@audenard.com>
2024-08-20 12:08:25 +0200

    feat(nvim): add telescope of harpoons

--- .config/nvim/lua/maurin/plugins/telescope.lua
+++ .config/nvim/lua/maurin/plugins/telescope.lua
@@ -29,14 +29,14 @@ return {
         -- Set keymaps
         local keymap = vim.keymap -- For conciseness
 
-        keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-        keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-        keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-        keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-        keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
-        keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find buffers" })
-        keymap.set("n", "<leader>fm", "<cmd>Telescope marks<cr>", { desc = "Find marks" })
-        keymap.set("n", "<leader>fw", "<cmd>Telescope current_buffer_fuzzy_find<cr>", { desc = "Find word in buffer" })
+        keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>",                  { desc = "Fuzzy find files in cwd" })
+        keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>",                    { desc = "Fuzzy find recent files" })
+        keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>",                   { desc = "Find string in cwd" })
+        keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>",                 { desc = "Find string under cursor in cwd" })
+        keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>",                         { desc = "Find todos" })
+        keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>",                     { desc = "Find buffers" })
+        keymap.set("n", "<leader>fm", "<cmd>Telescope harpoon marks<cr>",                       { desc = "Find marks" })
+        keymap.set("n", "<leader>fw", "<cmd>Telescope current_buffer_fuzzy_find<cr>",   { desc = "Find word in buffer" })
 
     end,
 }
